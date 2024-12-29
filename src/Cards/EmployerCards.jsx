808c095a266568371a3a762e67253a283f67ecf4
Sivapragasam <qc.siva@gmail.com>
2024-12-29 16:23:01 +0530

    usercontext

--- src/Cards/EmployerCards.jsx
+++ src/Cards/EmployerCards.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import userContext from '../UserContext';
 
 function EmployerCards({ employer }) {
   const { appliedCompany, setAppliedCompany } = useContext(userContext);
